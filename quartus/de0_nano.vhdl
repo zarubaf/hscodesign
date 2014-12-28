66e4be99fef7ae5a2499d3d5c492ca3dee62344a
Florian ZARUBA <w1025748@ti5.tilab.tuwien.ac.at>
2014-12-28 21:02:16 +0100

    HW Display added

--- quartus/de0_nano.vhdl
+++ quartus/de0_nano.vhdl
@@ -33,7 +33,16 @@ entity de0_nano is
 		AUD_BCLK :in std_logic;	
 		I2C_SDAT : inout std_logic;
 		I2C_SCLK : out std_logic;
-		AUD_MCLK : out std_logic
+		AUD_MCLK : out std_logic;
+		-- display
+		ltm_r                           : out   std_logic_vector(7 downto 0);
+		ltm_g                           : out   std_logic_vector(7 downto 0);
+		ltm_b                           : out   std_logic_vector(7 downto 0);
+		ltm_den                         : out   std_logic;                                        -- vid_datavalid
+		ltm_vd                          : out   std_logic;                                        -- vid_v_sync
+		ltm_hd                          : out   std_logic;                                        -- vid_h_sync                                        -- vid_v
+		ltm_clk                         : out   std_logic;
+		ltm_rst                         : out   std_logic
 
 	);
 
@@ -71,10 +80,24 @@ architecture arch of de0_nano is
 				audio_ext_interface_DACLRCK : in std_logic;
 				audio_config_SDAT : inout std_logic;
 				audio_config_SCLK : out std_logic;
-				audio_mclk_clk : out std_logic
+				audio_mclk_clk : out std_logic;                                      
+				-- display
+            ltm_vid_data                    : out   std_logic_vector(23 downto 0);                    -- vid_data
+            ltm_underflow                   : out   std_logic;                                        -- underflow
+            ltm_vid_datavalid               : out   std_logic;                                        -- vid_datavalid
+            ltm_vid_v_sync                  : out   std_logic;                                        -- vid_v_sync
+            ltm_vid_h_sync                  : out   std_logic;                                        -- vid_h_sync
+            ltm_vid_f                       : out   std_logic;                                        -- vid_f
+            ltm_vid_h                       : out   std_logic;                                        -- vid_h
+            ltm_vid_v                       : out   std_logic;                                        -- vid_v
+            display_clk                     : out   std_logic                                         -- clk
         );
     end component ;
-
+	 
+	signal ltm_vd_int : std_logic;
+	signal ltm_hd_int : std_logic;
+	signal ltm_data : std_logic_vector(23 downto 0);
+	
 begin
 
     u0 : component de0_nano_nios2
@@ -106,9 +129,26 @@ begin
 				audio_ext_interface_DACLRCK => AUD_DACLRCK,
 				audio_config_SDAT =>I2C_SDAT,
 				audio_config_SCLK => I2C_SCLK,
-				audio_mclk_clk => AUD_MCLK
+				audio_mclk_clk => AUD_MCLK,                      
+				ltm_vid_data                    => ltm_data,                    -- vid_data
+            ltm_underflow                   => open,                                        -- underflow
+            ltm_vid_datavalid               => ltm_den,                                        -- vid_datavalid
+            ltm_vid_v_sync                  => ltm_vd_int,                                       -- vid_v_sync
+            ltm_vid_h_sync                  => ltm_hd_int,                                        -- vid_h_sync
+            ltm_vid_f                       => open,                                        -- vid_f
+            ltm_vid_h                       => open,                                        -- vid_h
+            ltm_vid_v                       => open,				-- vid_v
+				display_clk                     => ltm_clk
 				);
+				
+			ltm_rst <= buttons(0);  
+			ltm_vd <= not ltm_vd_int;
+			ltm_hd <= not ltm_hd_int;
 
+
+			ltm_r     <= ltm_data(7 downto 0);
+			ltm_g     <= ltm_data(15 downto 8);
+			ltm_b     <= ltm_data(23 downto 16);
 end architecture;
 
 
